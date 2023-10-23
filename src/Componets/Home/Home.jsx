a05a2f223da4ce581f87ccad5824231957440385
Mohamed Ashraf <mohmedasharf99962@gmail.com>
2023-10-23 17:25:32 +0300

    first commit

---
+++ src/Componets/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+import MiddleSlide from './homeComponets/MiddleSlide';
+import Products1 from './homeComponets/Products1';
+import TopSlide from './homeComponets/TopSlide';
+
+
+export default function Home() {
+
+  return <>
+    <TopSlide />
+    <MiddleSlide />
+    <Products1 />
+  </>
+}
---
+++ src/Componets/Products/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Products1 from '../Home/homeComponets/Products1'
+
+export default function Products() {
+
+  return <>
+  <Products1></Products1>
+  </>
+}
